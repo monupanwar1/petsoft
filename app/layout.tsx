8f04180034d7fca15f974a7124e94dc57cac2fa6
kunal <monupanwar5655@gmail.com>
2025-04-03 18:44:23 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "patsoft pet dayacare software",
@@ -25,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+       className={`${inter.className} text-sm text-zinc-900 bg-[#E5E8EC]`}
       >
         {children}
       </body>
