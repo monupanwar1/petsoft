42e0d8a8ee08eb7e419c202bee6637b360bfe6af
kunal <monupanwar5655@gmail.com>
2025-04-04 19:41:06 +0530

    refactoring

---
+++ app/(app)/app/dashboard/branding.tsx
@@ -0,0 +1,14 @@
+import H1 from "@/components/h1";
+
+export default function Branding() {
+  return (
+    <section>
+      <div className="flex text-white py-8 items-center justify-center">
+        <H1>
+          Pet <span className="font-semibold">Soft</span>
+        </H1>
+        <p className="text-lg opacity-80"> Manage your pet daycare with ease</p>
+      </div>
+    </section>
+  );
+}
--- app/(app)/app/dashboard/page.tsx
+++ app/(app)/app/dashboard/page.tsx
@@ -1,23 +1,9 @@
-import H1 from "@/components/h1";
+import Stats from '@/components/stats';
 
 export default function Page() {
   return (
     <main>
-        <section>
-      <div className="flex text-white py-8 items-center justify-center">
-        <H1>
-          Pet <span className="font-semibold">Soft</span>
-        </H1>
-        <p className="text-lg opacity-80"> Manage your pet daycare with ease</p>
-      </div>
-      </section>
-           
-           <section className="text-center">
-            <p className="text-2xl font-bold leading-8">2</p>
-            <p className="opacity-80">Current Guest</p>
-           </section>
-
-      
+      <Stats />
     </main>
   );
 }
