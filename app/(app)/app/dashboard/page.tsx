ce713dcc9d6de013958e03a732a4b6699a01129b
kunal <monupanwar5655@gmail.com>
2025-04-04 19:34:20 +0530

    dashboard page

--- app/(app)/app/dashboard/page.tsx
+++ app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,21 @@
 export default function Page() {
   return (
-    <div>Dashboards</div>
-  )
+    <main>
+        <section>
+      <div className="flex text-white py-8 items-center justify-center">
+        <h1 className="font-medium text-2xl leading-6">
+          Pet <span className="font-semibold">Soft</span>
+        </h1>
+        <p className="text-lg opacity-80"> Manage your pet daycare with ease</p>
+      </div>
+      </section>
+           
+           <section className="text-center">
+            <p className="text-2xl font-bold leading-8">2</p>
+            <p className="opacity-80">Current Guest</p>
+           </section>
+
+      
+    </main>
+  );
 }
