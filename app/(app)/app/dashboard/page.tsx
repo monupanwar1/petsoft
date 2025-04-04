3769990aa0d628b0d4e9be25abcacca35ce7513c
kunal <monupanwar5655@gmail.com>
2025-04-04 20:20:27 +0530

    grid

--- app/(app)/app/dashboard/page.tsx
+++ app/(app)/app/dashboard/page.tsx
@@ -13,14 +13,21 @@ export default function Page() {
         <Stats />
       </div>
 
-      <div className='grid grid-cols-3 grid-row-[45px_1fr]'>
-        <Searchform/>
-        <ContentBlock>
-          <PetList/>
-        </ContentBlock>
-        <ContentBlock>
-          <PetDetails/>
-        </ContentBlock>
+      <div className="grid grid-cols-3 grid-rows-[45px_1fr] gap-4 h-[600px]">
+        <div className="row-start-1 row-span-1 col-start-1 col-span-1">
+          <Searchform />
+        </div>
+        <div className="row-start-2 row-span-full col-start-1 col-span-1">
+          <ContentBlock>
+            <PetList />
+          </ContentBlock>
+        </div>
+
+        <div className='row-start-1 row-span-full col-start-2 col-span-full'>
+          <ContentBlock>
+            <PetDetails />
+          </ContentBlock>
+        </div>
       </div>
     </main>
   );
